581f98542c78294eb3f5067b06fcb467b89e6830
gangsterispovilas <83584238+gangsterispovilas@users.noreply.github.com>
2024-03-06 16:13:56 +0100

    Update main.cpp

--- robobot/raubase/src/main.cpp
+++ robobot/raubase/src/main.cpp
@@ -49,7 +49,7 @@ int main (int argc, char **argv)
     // turn on LED on port 16
     gpio.setPin(16, 1);
     // run the planned missions
-    //mission0.run();
+    mission0.run();
     //plan20.run();
     //plan21.run();
     //plan40.run();
